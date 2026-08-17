64522d3afc5754fc7313529e8c1672ccaf75224e
ST10447771-Siphesihle-Mngomezulu <167029257+st10447771@users.noreply.github.com>
2026-08-17 14:32:58 +0200

    Change HTML comments to JavaScript comments

--- combined working prototype/goafrica-prototype/js/tailwind-config-donor.js
+++ combined working prototype/goafrica-prototype/js/tailwind-config-donor.js
@@ -1,9 +1,9 @@
 
-        <!-- Code Attribution -->
-        <!-- This method was taken from stackoverflow -->
-        <!-- Link: https://stackoverflow.com/questions/74626693/how-to-install-tailwind-css-in-html-correctly -->
-        <!-- Author: Dipo Ahmed -->
-        <!--Author Profile: https://stackoverflow.com/users/11229002/dipo-ahmed -->
+        // Code Attribution 
+        // This method was taken from stackoverflow 
+        // Link: https://stackoverflow.com/questions/74626693/how-to-install-tailwind-css-in-html-correctly 
+        // Author: Dipo Ahmed 
+        // Author Profile: https://stackoverflow.com/users/11229002/dipo-ahmed 
         
 tailwind.config = {
         darkMode: "class",
